035bafab47aa89a9acf8f7f7c9ee1b8db3644bc2
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-18 10:03:51 -0500

    starting update on site

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (event) {
     // array with texts to type in typewriter
-    var dataText = ["Hi, my name is Jacob.", "Welcome to my website!"];
+    var dataText = ["Hi, my name is Jacob.", "Welcome to my website!", "I appreciate your patience while were under construction!"];
 
     // type one text in the typwriter
     // keeps calling itself until the text is finished
