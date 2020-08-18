e6640827c949e87fcd377e12b3dde6f1d394ba9c
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-18 10:02:07 -0500

    starting update on site

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded',function(event){
+document.addEventListener('DOMContentLoaded', function (event) {
     // array with texts to type in typewriter
-    var dataText = [ "Hi, my name is Jacob.", "I am a Full Stack Web Developer from Dallas TX", "My passion is to create", "Welcome to my website!"];
+    var dataText = ["Hi, my name is Jacob.", "Welcome to my website!"];
 
     // type one text in the typwriter
     // keeps calling itself until the text is finished
@@ -8,10 +8,10 @@ document.addEventListener('DOMContentLoaded',function(event){
         // chekc if text isn't finished yet
         if (i < (text.length)) {
             // add next character to h1
-            document.querySelector("h3").innerHTML = text.substring(0, i+1) +'<span class="animate" aria-hidden="true"></span>';
+            document.getElementById("move-text").innerHTML = text.substring(0, i + 1) + '<span class="animate" aria-hidden="true"></span>';
 
             // wait for a while and call this function again for next character
-            setTimeout(function() {
+            setTimeout(function () {
                 typeWriter(text, i + 1, fnCallback)
             }, 100);
         }
@@ -21,22 +21,24 @@ document.addEventListener('DOMContentLoaded',function(event){
             setTimeout(fnCallback, 700);
         }
     }
+
     // start a typewriter animation for a text in the dataText array
     function StartTextAnimation(i) {
-        if (typeof dataText[i] == 'undefined'){
-            setTimeout(function() {
+        if (typeof dataText[i] == 'undefined') {
+            setTimeout(function () {
                 StartTextAnimation(0);
             }, 20000);
         }
         // check if dataText[i] exists
         if (i < dataText[i].length) {
             // text exists! start typewriter animation
-            typeWriter(dataText[i], 0, function(){
+            typeWriter(dataText[i], 0, function () {
                 // after callback (and whole text has been animated), start next text
                 StartTextAnimation(i + 1);
             });
         }
     }
+
     // start the text animation
     StartTextAnimation(0);
 });
